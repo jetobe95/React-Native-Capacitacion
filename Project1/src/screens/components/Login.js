e1eeffca87b93afbe8c1ccbba86b75b982492bf0
fabirt <fandig27@gmail.com>
2018-10-31 14:26:21 -0500

    login screen

--- Project1/src/screens/components/Login.js
+++ Project1/src/screens/components/Login.js
@@ -6,6 +6,7 @@ import {
   SafeAreaView,
   TextInput,
   Button,
+  TouchableOpacity
  } from 'react-native';
 import { LinearGradient } from 'expo';
 
@@ -14,22 +15,32 @@ class Login extends Component {
     return (
       <LinearGradient colors={['#523fd9', '#843893']} style={{flex: 1}}>
         <SafeAreaView style={styles.container}>
-            <View >
+            <View style={styles.formSection}>
                 <Text style={styles.titleText}>PROJECT</Text>
-                <TextInput
-                    style={{height: 40}}
-                    placeholder="Username"
-                />
-                <TextInput
-                    style={{height: 40}}
-                    placeholder="Password"
-                />
-                <Button 
-                    title="SIGN IN"
-                    onPress = {() => {
-                        //continue
-                    }}
-                />
+                <View>
+                    <TextInput
+                        style={styles.editText}
+                        placeholder="Username"
+                        selectionColor={'#fff'}
+                        underlineColorAndroid={'#ddd'}
+                    />
+                    <TextInput
+                        style={styles.editText}
+                        placeholder="Password"
+                        selectionColor={'#fff'}
+                        underlineColorAndroid={'#ddd'}
+                    />
+                </View>
+                <View style={styles.button1}>
+                    <Button 
+                        title="SIGN IN"
+                        color={'purple'}
+                        onPress = {() => {
+                            //continue
+                        }}
+                    />
+                </View>
+                <Text style={styles.signUp}>Don't have an account yet? Sign Up</Text>
             </View>
         </SafeAreaView>
       </LinearGradient>
@@ -41,12 +52,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
   titleText: {
-      fontSize: 28,
-      color: '#fff',
-      fontWeight: 'bold',
+    fontSize: 28,
+    color: '#fff',
+    fontWeight: 'bold',
+    margin: 60,
+  },
+  formSection: {
+    flex: 2,
+    justifyContent: 'center'
+  },
+  editText: {
+    fontSize: 20,
+    color: '#fff',
+    padding: 14,
+    width: 240,
+    marginTop: 10,
+    marginBottom: 16,
+    
+  }, 
+  button1: {
+      marginTop: 50,
+  },
+  signUp: {
+      marginTop: 32,
+      color: 'rgba(255,255,255,0.6)',
   }
 });
 
