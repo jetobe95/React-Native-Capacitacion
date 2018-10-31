4398bb2f5c473db413505ce83e273fe095d145ac
fabirt <fandig27@gmail.com>
2018-10-31 11:34:04 -0500

    start project

--- Project1/App.js
+++ Project1/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <LinearGradient colors={['blue', 'tomato']}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </LinearGradient>
       </View>
     );
   }
